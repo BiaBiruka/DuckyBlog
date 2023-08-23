d96b2b27c7bc17e6eaa15bd088f6b51c4699ca88
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2023-08-23 10:34:29 -0300

    :tada: Initial Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import styles from "./Navbar.module.css";
+
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavLink to="/">
+        <img src="" />
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
