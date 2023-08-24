5a2d385f4bac370ff928bc5c57eeb8a779d30ad1
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2023-08-24 13:52:21 -0300

    Navbar and footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <h3>Pense. Escreva. Inspire. </h3>
+      <p>DuckyBlog &copy; 2023</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,49 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={styles.navbar}>
       <NavLink to="/">
-        <img src="" />
+        <div className={styles.logo}>
+          <img src="/ducky.svg" alt="DuckyBlog logo" />
+          <div className={styles.logoText}>
+            Ducky<span>Blog</span>
+          </div>
+        </div>
       </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Menu
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/sobre"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Sobre
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/cadastro"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Cadastro
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/login"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Login
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
