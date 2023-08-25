bc5a927a8139139816e88ccdabd8bd8cbe060fa4
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2023-08-25 14:07:36 -0300

    Visual changes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,13 +5,12 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div className={styles.aboutPage}>
-      <div className={styles.logo}>
-        <h2>
-          <span className={styles.sobre}>Sobre o</span>
-          <img src="/ducky.svg" alt="DuckyBlog logo" />
-          Ducky<span className={styles.blog}>Blog</span>
-        </h2>
-      </div>
+      <h2 className={styles.title}>
+        <span>
+          Sobre o <span className={styles.ducky}>Ducky</span>
+          <span className={styles.blog}>Blog</span>
+        </span>
+      </h2>
 
       <p>
         Este projeto consiste em um blog feito com React no Front-end e Firebase
