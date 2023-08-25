2721d27a7582529a729f135551238b721dcfb598
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2023-08-25 13:39:49 -0300

    About page done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,25 @@
 import styles from "./About.module.css";
 
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.aboutPage}>
+      <div className={styles.logo}>
+        <h2>
+          <span className={styles.sobre}>Sobre o</span>
+          <img src="/ducky.svg" alt="DuckyBlog logo" />
+          Ducky<span className={styles.blog}>Blog</span>
+        </h2>
+      </div>
+
+      <p>
+        Este projeto consiste em um blog feito com React no Front-end e Firebase
+        no Back-end.
+      </p>
+      <Link to="/postagens/novo" className="btn">
+        Comece a postar agora!
+      </Link>
     </div>
   );
 };
