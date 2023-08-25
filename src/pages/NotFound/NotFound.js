2907bf26dc9aa2704d718889b7281b404be33365
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2023-08-25 13:42:22 -0300

    Not Found page created

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import styles from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Página não encontrada</h1>
+    </div>
+  );
+};
+
+export default NotFound;
