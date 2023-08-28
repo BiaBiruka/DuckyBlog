e19ac6f5e49b2ec43f49e1fd0974bf949e47bf4e
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2023-08-28 16:01:10 -0300

    Menu now show the posts

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+  QuerySnapshot,
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // Memody leak
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    const loadData = async () => {
+      if (cancelled) return;
+      setLoading(true);
+
+      // Cria a referência de documento
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        // Querys mais complexas
+        let q;
+        if (search) {
+          // Busca de acordo com a tag escrita no campo de pesquisa
+          q = await query(
+            collectionRef,
+            where("tags", "array-contains", search),
+            orderBy("createdAt", "desc")
+          );
+        } else {
+          // Caso não tenha sido pesquisado nada, exibe tudo
+          q = await query(collectionRef, orderBy("createdAt", "desc"));
+        }
+
+        // Mantém os dados atualizados - sempre que alterar, trás
+        await onSnapshot(q, (querySnapshot) => {
+          // Os dados vem do firebase com coisas extras. Fazemos um mapa com todos os documentos e então um mapa de cada documento
+          setDocuments(
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    loadData();
+  }, [docCollection, search, uid, cancelled]); //Se chegar um deles já pode buscar dados
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return { documents, loading, error };
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,49 @@
+// CSS
 import styles from "./Home.module.css";
 
+// Hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
+// Componentes
+import PostDetail from "../../components/PostDetail";
+
 const Home = () => {
+  const [query, setQuery] = useState();
+  const { documents: posts, loading } = useFetchDocuments("posts");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (query) {
+      // Se tem query, manda para uma página do search feito
+      return navigate(`/pesquisa?q=${query}`);
+    }
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Postagens mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Pesquisar por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-search">Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando dados...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+        {posts && posts.length === 0 && (
+          <div className={styles.no_posts}>
+            <p>Nenhuma postagem foi encontrada...</p>
+            <Link to="/postagens/novo" className="btn">
+              Crie um agora!
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
