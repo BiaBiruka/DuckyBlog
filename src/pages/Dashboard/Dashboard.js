6130c0378cc25fea45a9b540a8a78998684ea9fc
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2023-08-29 14:40:54 -0300

    User's posts dashboard added

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,33 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+// Contextos e Hooks
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  // Postagens do usuário
+  const posts = [];
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className={styles.dashboard}>
+      <h1>Painel de postagens</h1>
+      {posts && posts.length === 0 ? (
+        <div className={styles.no_posts}>
+          <p>Você ainda não tem postagens publicadas.</p>
+          <Link to="/postagens/novo" className="btn">
+            Crie uma agora!
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Postagens:</p>
+        </div>
+      )}
     </div>
   );
 };
